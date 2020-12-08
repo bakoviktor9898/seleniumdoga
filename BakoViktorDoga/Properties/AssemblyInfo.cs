cfaea0b7cf4ab86d818f8350f570b230baa9c046
bakoviktor <bako.viktor9898@gmail.com>
2020-12-08 14:38:11 +0100

    dolgozat

---
+++ BakoViktorDoga/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BakoViktorDoga")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BakoViktorDoga")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0a81414-7c27-41d2-ba38-23d1b6fd76df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BakoViktorDoga/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace BakoViktorDoga
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver dw = new FirefoxDriver();
+
+            dw.Navigate().GoToUrl("http://atthy4.atspace.cc/seleniumdoga/Hivatalosdoga.html");
+            dw.Manage().Window.Maximize();
+
+            dw.FindElement(By.LinkText("DevOps 1")).Click();
+
+            dw.Navigate().Back();
+            // 2 feladat ket jatekos
+            var drop = dw.FindElement(By.Name("visitor[b]"));
+            var select = new SelectElement(drop);
+            var drop2 = dw.FindElement(By.Name("visitor[a]"));
+            var select2 = new SelectElement(drop2);
+            select.SelectByIndex(3);
+            select2.SelectByIndex(4);
+            //2. feladat checkboxos
+            IWebElement checkbox = dw.FindElement(By.Id("checkbox_car"));
+            if (!checkbox.Selected)
+            {
+                checkbox.Click();
+            }
+
+            Assert.IsTrue(checkbox.Selected);
+
+            // 3 feladat
+            
+
+            //4 feladat
+            IWebElement elem = dw.FindElement(By.XPath("/html/body/button"));
+            elem.Click();
+            Assert.AreEqual("Minden Pontot megszerzek", dw.Title);
+
+            //5.
+            var radio = dw.FindElement(By.Id("radio_kivalo"));
+            radio.Click();
+            Assert.IsFalse(dw.FindElement(By.Id("radio_jeles")).Selected);
+
+            dw.FindElement(By.TagName("button")).Click();
+            int kepek = dw.FindElements(By.TagName("img")).Count;
+            Assert.IsTrue(kepek==2);
+        }
+    }
+}
